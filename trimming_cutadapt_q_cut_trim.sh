215b0599bc9dbe3c3043db05c7243f37457dd6c9
meganhartnett <megan.ni.airtneada@gmail.com>
2022-06-28 23:10:32 +0100

    My script after deleting not 100% if right

--- trimming_cutadapt_q_cut_trim.sh
+++ trimming_cutadapt_q_cut_trim.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #$ -cwd
 #$ -V
-#$ -N Cutadapt_trim_3030
+#$ -N Cutadapt_trim_2
 #$ -l h_rt=06:00:00
-#$ -l h_vmem=64G
+#$ -l h_vmem=16G
 
 module load igmm/apps/anaconda/5.0.0.1
 module load igmm/apps/cutadapt/1.16
@@ -12,6 +12,6 @@ cd  /exports/eddie/scratch/s2210624/tr_trimming_qc/
 for file in $(cat tr2.txt)
 do
 f_name=$(basename $file _1.fastq.gz)
-cutadapt --quality-cutoff 30,30 $file | cutadapt -l 34 - | cutadapt -g XNNNCAGCAG - -m 25 --untrimmed-output "$f_name"_untrimmed_cutadapt_q3030_cut_34_trim.txt --too-short-output "$f_name"_too_short_cutadapt_q3030_cut_34_trim.txt -o "$f_name"_cutadapt_q3030_cut_34_trim.fastq.gz
+cutadapt --quality-cutoff 30 $file | cutadapt -l 34 - | cutadapt -g "XNNNCAGCAG;e=2.5"  - | cutadapt -m 25 - -o "$f_name"_cutadapt_q_e2.5.fastq.gz
 done
 cd /exports/cmvm/eddie/sbms/groups/young-lab/megan
