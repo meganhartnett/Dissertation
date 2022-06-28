c6843f0be78e6ccd07f1631efc8cb72dc1e69e0b
meganhartnett <megan.ni.airtneada@gmail.com>
2022-06-28 11:56:18 +0100

    The second cutadapt script- the quality filtering done first- not good

---
+++ trimming_cutadapt_q_first.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -cwd
+#$ -V
+#$ -N Cutadapt_trim_2
+#$ -l h_rt=06:00:00
+#$ -l h_vmem=16G
+
+module load igmm/apps/anaconda/5.0.0.1
+module load igmm/apps/cutadapt/1.16
+source activate /exports/cmvm/eddie/sbms/groups/young-lab/megan/.conda/envs/CAGE2r
+cd  /exports/eddie/scratch/s2210624/tr_trimming_qc/
+for file in $(cat tr2.txt)
+do
+f_name=$(basename $file _1.fastq.gz)
+cutadapt --quality-cutoff 30 $file | cutadapt -a NNTCGTATGCCGTCTTCTGCTTG -g NNNCAGCAG - | cutadapt -l 25 - -o "$f_name"_cutadapt_q_first.fastq.gz
+done
+cd /exports/cmvm/eddie/sbms/groups/young-lab/megan
